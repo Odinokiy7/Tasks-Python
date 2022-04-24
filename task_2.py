c47c84c906dde18cf68ca156808ed7cdbd60da03
Odinokiy7 <koazo-maga@mail.ru>
2022-04-24 13:48:10 +0300

    Task 2.2

--- task_2.py
+++ task_2.py
@@ -1,8 +1,8 @@
-#2. Найти максимальное из пяти чисел
+# Найти максимальное из пяти чисел
 
 array = [1, 62, 6, 41, 15]
 max = array[0]
 for count in array:
     if count > max:
         max = count
-print(f"Максимальное число -", max)
+print(f'Максимальное число -', max)
